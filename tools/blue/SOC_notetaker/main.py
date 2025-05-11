2a855ee560e5a8b622d395af63d9cdde5b6c3315
Alex <125412758+jenkaj-lab@users.noreply.github.com>
2025-05-11 08:01:04 +0100

    Create main.py

---
+++ tools/blue/SOC_notetaker/main.py
@@ -0,0 +1,9 @@
+import sys
+from PyQt5 import QtWidgets
+from classes.main_window import MainWindow
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    sys.exit(app.exec_())
