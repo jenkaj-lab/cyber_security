c55ceb3cf8878d071be6a35f519485b3acccd2e1
Alex <125412758+jenkaj-lab@users.noreply.github.com>
2025-05-11 08:23:17 +0100

    Create api_keys.py

---
+++ tools/blue/SOC_notetaker/api_keys.py
@@ -0,0 +1,35 @@
+IPINFO_ACCESS_TOKEN = (
+    # https://ipinfo.io/
+    # 50,000 lookups per month
+    "Sign up for the API key and input it here"
+)
+
+ABUSE_IPDB_API_KEY = (
+    # https://www.abuseipdb.com/
+    # 1,000 lookups per day
+    "Sign up for the API key and input it here"
+)
+
+IPDATA_API_KEY = (
+    # https://ipdata.co/
+    # 1,500 lookups per day
+    "Sign up for the API key and input it here"
+)
+
+VIRUSTOTAL_API_KEY = (
+    # https://www.virustotal.com/
+    # 4 lookups per minute, 500 lookups per day, 15k lookups per month
+    "Sign up for the API key and input it here"
+)
+
+VPN_API_KEY = (
+    # https://vpnapi.io/
+    # 1,000 lookups per day
+    "Sign up for the API key and input it here"
+)
+
+PROXYCHECK_API_KEY = (
+    # https://proxycheck.io/
+    # 1,000 lookups per day
+    "Sign up for the API key and input it here"
+)
