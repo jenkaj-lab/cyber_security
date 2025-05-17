511a8f179034837f9d3573b76491dabd17a7ebb7
alex <jenkins.alexanderj@gmail.com>
2025-05-17 16:26:09 -0100

    reqs

--- tools/blue/SOC_notetaker/api_keys.py
+++ tools/blue/SOC_notetaker/api_keys.py
@@ -1,35 +1,35 @@
 IPINFO_ACCESS_TOKEN = (
     # https://ipinfo.io/
     # 50,000 lookups per month
-    "Sign up for the API key and input it here"
+    "f209f59c2e9848"
 )
 
 ABUSE_IPDB_API_KEY = (
     # https://www.abuseipdb.com/
     # 1,000 lookups per day
-    "Sign up for the API key and input it here"
+    "ae76f0b52fa7aae06c5a2a9de0f0bef861917129e13c0324e3b7aa533df68d8780def548bcebc97d"
 )
 
 IPDATA_API_KEY = (
     # https://ipdata.co/
     # 1,500 lookups per day
-    "Sign up for the API key and input it here"
+    "c7ff91f007bad60c7ffebc84199ef43f74d5a3fece243616fb4fd21f"
 )
 
 VIRUSTOTAL_API_KEY = (
     # https://www.virustotal.com/
     # 4 lookups per minute, 500 lookups per day, 15k lookups per month
-    "Sign up for the API key and input it here"
+    "bcb8bd677b2c218c40b78f1fc5a93c4c912a15ddca77fe33a077968bb609f83b"
 )
 
 VPN_API_KEY = (
     # https://vpnapi.io/
     # 1,000 lookups per day
-    "Sign up for the API key and input it here"
+    "13508c6ae9c541878dd57f93f21b706b"
 )
 
 PROXYCHECK_API_KEY = (
     # https://proxycheck.io/
     # 1,000 lookups per day
-    "Sign up for the API key and input it here"
+    "p4zv21-q9642z-6c168u-01429h"
 )
